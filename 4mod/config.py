458c9d60cad304a39a03b10c8451a618140aa133
Alex <alex.berdovsky@gmail.com>
2018-02-05 20:20:59 +0000

    Add files via upload

---
+++ 4mod/config.py
@@ -0,0 +1,6 @@
+tg_channel_name = '@tstgrp'
+vk_short_link='ru9gag'
+tg_bot_token= '408228757:AAHCKrCxI2G12HFRzAuU9KxR-K34u4lnGUM'
+vk_token = '155d155145f70d3409dfdcd67074ffd7a30becac64b23dd7b52c6b74ad71b6017574b15c10f6dc27eb219'
+sleep_time_min = 1
+CREATOR = '@GalaxyDragon'
